ad591d894af099162b1db978f3cd36f06de428f3
Ian Burden <ian.burden@indigobravo.com>
2021-07-15 09:00:32 +0100

    lms-viahtml-qa - platform extension testing
    This commit delivers a series of platform extensions to lms-viahtml-qa
    that will ultimately become common extensions installed to all
    environments.
    
    New extensions...
    - lms-viahtml/platform/qa/hooks/prebuild/01_newrelic_setup.sh
    - lms-viahtml/platform/qa/hooks/prebuild/02_via_firewall.sh
    - lms-viahtml/platform/qa/hooks/predeploy/01_iamsync_setup.sh
    
    That replace...
    - common/ebextensions/100_newrelic-infra.config
    - lms-viahtml/ebextensions/qa/02_iamsync.config
    - lms-viahtml/ebextensions/qa/50_iptables.config

---
+++ lms-viahtml/platform/qa/hooks/predeploy/01_iamsync_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# 01_iamsync_setup.sh
+#
+# Setup and configure iamsync for use with Elastic Beanstalk.
+set -eu
+
+cat <<CRON > /etc/cron.d/iamsync
+SHELL=/bin/bash
+PATH="venv/bin:/usr/sbin:/bin"
+5,35 * * * * root sleep \${RANDOM: -1} ; python bin/iamsync.py
+CRON
+
+cat <<CONFIG > /etc/iamsync.yml
+iamsync:
+  - iam_group: engineering
+    sudo_rule: "ALL=(ALL) NOPASSWD:ALL"
+    local_gid: 1025
+CONFIG
+
+mkdir -p /root/bin
+curl https://raw.githubusercontent.com/hypothesis/iamsync/main/iamsync.py -o /root/bin/iamsync.py
+
+python3 -m venv /root/venv
+source /root/venv/bin/activate ; pip install wheel boto3 pyyaml
+
+/root/venv/bin/python /root/bin/iamsync.py
